5196291f020cc708d4a2091cdf07a8f7a27c2366
Kevin Tang <kevin.tang@twosigma.com>
2021-03-28 17:35:06 -0500

    create room button

--- src/gameView/gamePlayer/GamePlayer.tsx
+++ src/gameView/gamePlayer/GamePlayer.tsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import {
-  Theme, withStyles, createStyles, Typography,
+  Theme, withStyles, createStyles, Typography, List, Button,
 } from '@material-ui/core';
 import {
   useParams,
 } from 'react-router-dom';
+
 import { Game, getGame } from '../../models/game';
 import IFrame from './IFrame/IFrame';
+import { createRoom, getRooms, Room } from '../../models/room';
 
 type Props = {
   classes: any
@@ -19,23 +21,51 @@ type GamesURLParams = {
 const GamePlayer = ({ classes }: Props) => {
   const { gameId } = useParams<GamesURLParams>();
   const [game, setGame] = useState<null | Game>(null);
+  const [rooms, setRooms] = useState<Room[]>([]);
 
   useEffect(() => {
     async function setupGame() {
       const gameRaw = await getGame(gameId);
       setGame(gameRaw);
     }
+    async function setupRooms() {
+      const roomsRaw = await getRooms(gameId);
+      setRooms(roomsRaw);
+    }
     setupGame();
+    setupRooms();
   }, []);
 
   if (game) {
+    const gitHubInfo = `GitHub Info: ${game.githubURL} ${game.commitSHA}`;
+    // TODO: display rooms and way to join room
+    // TODO: form to create a room
     return (
       <div className={classes.root}>
         <Typography variant="h3">
           {game.name}
         </Typography>
         <Typography variant="body1">{game.description}</Typography>
-        <IFrame src={game.githubURL} commitSHA={game.commitSHA} />
+        <Typography variant="body1">{gitHubInfo}</Typography>
+        <Button
+          variant="contained"
+          disableElevation
+          className={classes.button}
+          onClick={async (ev) => {
+            ev.preventDefault();
+            const room = await createRoom(game.id);
+            console.log('room created', room);
+          }}
+        >
+          Create Room
+        </Button>
+        {rooms.length > 0
+        && (
+        <List>
+          hello
+        </List>
+        )}
+        <IFrame githubURL={game.githubURL} commitSHA={game.commitSHA} />
       </div>
     );
   }
@@ -53,6 +83,9 @@ const styles = (theme: Theme) => createStyles({
     justifyContent: 'space-around',
     backgroundColor: theme.palette.background.paper,
     margin: '0 auto 0 auto',
+  },
+  button: {
+    margin: '5px',
   },
 });
 
