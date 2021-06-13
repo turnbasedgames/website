a1c85ae93c0b44ecfbb9a969aa2cba26767c969f
Kevin <kevin26428@gmail.com>
2021-06-13 17:03:37 -0500

    add browserfs

--- src/gameView/gameEditor/GameEditor.tsx
+++ src/gameView/gameEditor/GameEditor.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Editor from '@monaco-editor/react';
 
-const GameEditor = () => (
-  <div style={{ margin: '70px' }}>
-    <Editor
-      height="80vh"
-      defaultLanguage="javascript"
-      defaultValue="// some comment"
-    />
-  </div>
-);
+const GameEditor = () => {
+  useEffect(() => {
+    const fs = window.require('fs');
+    const files = fs.readdirSync('/', { withFileTypes: true });
+    console.log(files);
+    console.log('test');
+  }, []);
+  return (
+    <div style={{ margin: '70px' }}>
+      <Editor
+        height="80vh"
+        defaultLanguage="javascript"
+        defaultValue="// some comment"
+      />
+    </div>
+  );
+};
 
 export default GameEditor;
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,18 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const BrowserFS = require('browserfs');
+
+BrowserFS.install(window);
+BrowserFS.configure({
+  fs: 'LocalStorage',
+  options: {},
+}, (e: Error) => {
+  if (e) {
+    throw e;
+  }
+});
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
