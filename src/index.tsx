e3bce91520d17853a09c6e7514117d8a4e6d76c5
Kevin Tang <kevin.tang@twosigma.com>
2021-06-19 23:04:01 -0500

    remove browserfs and add firebase storage

--- src/index.tsx
+++ src/index.tsx
@@ -4,18 +4,6 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const BrowserFS = require('browserfs');
-
-BrowserFS.install(window);
-BrowserFS.configure({
-  fs: 'LocalStorage',
-  options: {},
-}, (e: Error) => {
-  if (e) {
-    throw e;
-  }
-});
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
